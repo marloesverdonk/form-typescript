2c91f6bf2dfc6ac1e2300c463052f0354ce17d59
marloesverdonk <marloesverdonk@live.nl>
2020-02-06 16:48:17 +0100

    Toggle password button and checkbox to accept terms work

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,12 +4,17 @@ interface Props {
     onSubmit: any
     onChange: any
     values: any
+    showPassword: any
+    onClickPassword: any
+    termsAccepted: any
+    onClickTerms: any
 }
 
 export const Form: React.FC<Props> = (props) => {
     return (
         <div>
             <form onSubmit={props.onSubmit}>
+                <label></label>
                 <input
                     name='email'
                     type='text'
@@ -18,26 +23,50 @@ export const Form: React.FC<Props> = (props) => {
                     value={props.values.email}
                 >
                 </input>
+
+                <br></br>
+
                 <input
                     name='password'
-                    type='text'
+                    type={props.showPassword ? 'text' : 'password'}
                     placeholder='password'
                     onChange={props.onChange}
                     value={props.values.password}
                 >
                 </input>
+
+                <br></br>
+
                 <input
                     name='confirmPassword'
-                    type='text'
+                    type={props.showPassword ? 'text' : 'password'}
                     placeholder='confirm password'
                     onChange={props.onChange}
                     value={props.values.confirmPassword}
                 >
                 </input>
+
+                <br></br>
+
+                <button type='button' onClick={props.onClickPassword}>Show password</button>
+
+                <br></br>
+                <br></br>
+
+                <label>Accept terms</label>
+                <input
+                    type="checkbox"
+                    onClick={props.onClickTerms}
+                />
+
+                <br></br>
+
                 <button type='submit'>
                     Submit
                 </button>
             </form>
+            <div>
+            </div>
         </div>
     )
 }
